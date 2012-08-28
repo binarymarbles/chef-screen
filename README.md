788334435a007fb65f6d31b6ddd9e4c3b4769673
Trond Arve Nordheim <tanordheim@gmail.com>
2012-08-28 14:47:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## DESCRIPTION
+
+Installs and configures screen on a node.
+
+## REQUIREMENTS
+
+Only tested on Debian 6.0.
+
+## USAGE
+
+The screen:default recipe installs screen on the node.
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+maintainer       'Trond Arve Nordheim'
+maintainer_email 't@binarymarbles.com'
+license          'Apache 2.0'
+description      'Installs and configures screen'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
+
+supports         'debian'
+
+recipe           'screen', 'Installs and configures screen'
